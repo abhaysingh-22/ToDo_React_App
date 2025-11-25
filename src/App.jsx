a3f333d5331669c4626d71cc8ff9084f6c9b0566
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-11-25 16:30:55 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ import Left from './components/Left.jsx';
 import Upcoming from './components/Upcoming';
 import Today from './components/Today';
 import Calendar from './components/Calendar';
-import StickyWall from './components/StickyWall';
 import WebViewer from './components/WebViewer.jsx';
-import ChatBot from './components/chatbot.jsx';
 
 export default function App() {
   const { Hide, setHide } = useContext(TodoContext)
@@ -27,14 +25,12 @@ export default function App() {
         <div className="body flex gap-1 h-[100lvh] overflow-x-hidden overflow-y-hidden bg-white  dark:bg-gray-900 transition-colors duration-200">
           <div className='h-[99lvh]'>
             <Left />
-            <ChatBot/>
           </div>
           <div className={`right w-[79dvw] h-[100lvh] overflow-x-hidden  max-md:w-[88dvw] max-lg:w-[94dvw] dark:text-gray-200 transition-all duration-200 ease-in-out ${Hide ? "w-full" : "w-[79vw]"} `}>
             <Routes>
               <Route path="/" element={<Upcoming />} />
               <Route path="/today" element={<Today />} />
               <Route path="/Calendar" element={<Calendar />} />
-              {/* <Route path="/StickyWall" element={<StickyWall />} /> */}
               <Route path="/web/:site" element={<WebViewer />} />
             </Routes>
           </div>
